61233d8c0ee523991493704d7afd868f6ad7c925
kArA <kArARedwing@gmail.com>
2026-04-03 08:13:48 -0400

    allow toggles to color swap

--- assets/scripts/landing.js
+++ assets/scripts/landing.js
@@ -1,4 +1,23 @@
+const SortType = Object.freeze({
+	COMMON: 'common',
+	SCIENTIFIC: 'scientific'
+});
+
 const test = d3.select('#test');
 // const link = `${R2_BUCKET}/${Bucket.BIVALVE}/${FIRST_LINK}`
 const link = '../test-image.jpg'
 test.attr(`src`, link);
+
+//  Sort
+function sortBy (type) {
+	const commonButton = d3.select('#sort-by-common');
+	const scientificButton = d3.select('#sort-by-scientific');
+
+	if (type === SortType.SCIENTIFIC) {
+		commonButton.classed('active-toggle', false);
+		scientificButton.classed('active-toggle', true);
+	} else  {
+		commonButton.classed('active-toggle', true);
+		scientificButton.classed('active-toggle', false);
+	}
+}
